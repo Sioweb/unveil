cca6149fd6a07bb6869a64d81ba013ee750c70bf
Sascha Weidner <support@sioweb.de>
2019-03-12 15:09:53 +0100

    Update jquery.unveil.js

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -8,8 +8,23 @@
  * https://github.com/luis-almeida
  */
 
-;(function($) {
-
+(function (root, factory) {
+	if (root === undefined && window !== undefined) root = window;
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module unless amdModuleId is set
+		define(["jquery"], function (a0) {
+			return (factory(a0));
+		});
+	} else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory(require("jquery"));
+	} else {
+		factory(root["jQuery"]);
+	}
+}(this, function (jQuery) {(function ($) {
+  'use strict';
   $.fn.unveil = function(threshold, callback) {
 
     var $w = $(window),
@@ -50,5 +65,5 @@
     return this;
 
   };
-
-})(window.jQuery || window.Zepto);
+})(jQuery);
+}));
